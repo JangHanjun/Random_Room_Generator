4d0bd20d2bf0fa6c3f12631d99232459fcf294d0
husk <huskgit@gmail.com>
2021-01-27 19:11:36 +0900

    2021.01.27

--- Random_Room_Generator/Assets/RandomPlatform/LevelGeneration.cs
+++ Random_Room_Generator/Assets/RandomPlatform/LevelGeneration.cs
@@ -17,8 +17,11 @@ public class LevelGeneration : MonoBehaviour
     public float minX;
     public float maxX;
     public float minY;
-    private bool stopGeneration = false;    // 경계선 체크 후 범위 벗어나면 더이상 생성 x
+    public bool stopGeneration;    // 경계선 체크 후 범위 벗어나면 더이상 생성 x
     public LayerMask room; //이전 방을 탐색하기 위한 변수
+    //
+    private int downCounter;
+
     private void Start() {
         int randStartingPos = Random.Range(0, startingpositions.Length);
         transform.position = startingpositions[randStartingPos].position;
@@ -38,6 +41,7 @@ public class LevelGeneration : MonoBehaviour
 
     private void Move(){
         if(direction == 1 || direction == 2){   // 방 생성기가 오른쪽
+            downCounter = 0;
             if(transform.position.x < maxX){    // 범위를 초과했다면
                 Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
                 transform.position = newPos;
@@ -58,6 +62,7 @@ public class LevelGeneration : MonoBehaviour
             }
         }else if(direction == 3 || direction == 4){// 방 생성기가 왼쪽
             if(transform.position.x > minX){        // 범위를 초과했다면
+                downCounter = 0;
                 Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
                 transform.position = newPos;
 
@@ -72,17 +77,26 @@ public class LevelGeneration : MonoBehaviour
                 direction = 5;
             }
             
-        } else{// 방 생성기가 아래
+        } else if(direction == 5){// 방 생성기가 아래
+            downCounter++;
+
             if(transform.position.y > minY){
                 Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
                 if(roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3){
-                    // 바닥이 없다는 뜻
-                    roomDetection.GetComponent<RoomType>().RoomDestruction(); // 삭제
-                    int randBottomRoom = Random.Range(1,4);
-                    if(randBottomRoom == 2){
-                        randBottomRoom = 1;
+                    
+                    if(downCounter >= 2){
+                        roomDetection.GetComponent<RoomType>().RoomDestruction(); // 삭제
+                        Instantiate(rooms[3], transform.position, Quaternion.identity);
+                    } else{
+                        // 바닥이 없다는 뜻
+                        roomDetection.GetComponent<RoomType>().RoomDestruction(); // 삭제
+
+                        int randBottomRoom = Random.Range(1,4);
+                        if(randBottomRoom == 2){
+                            randBottomRoom = 1;
+                        }
+                        Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
                     }
-                    Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
                 }
 
                 Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
