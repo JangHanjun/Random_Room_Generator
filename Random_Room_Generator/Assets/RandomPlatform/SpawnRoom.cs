f51aa62e8390641dda8355823297cf4bf2fd154e
husk <huskgit@gmail.com>
2021-01-27 19:17:54 +0900

    2021.01.27

--- Random_Room_Generator/Assets/RandomPlatform/SpawnRoom.cs
+++ Random_Room_Generator/Assets/RandomPlatform/SpawnRoom.cs
@@ -8,7 +8,7 @@ public class SpawnRoom : MonoBehaviour
     public LevelGeneration levelGen;
     void Update(){
         Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, whatIsRoom);
-
+        // 감지되는 방이 없다 = 여긴 생성이 안되엇다 > 아무 방이나 생성하기
         if(roomDetection == null && levelGen.stopGeneration == true){
             int rand = Random.Range(0, levelGen.rooms.Length);
             Instantiate(levelGen.rooms[rand], transform.position, Quaternion.identity);
